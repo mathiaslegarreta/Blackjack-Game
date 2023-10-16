5664ede32270437ada382955858e90600c1ed3af
Mathias Legarreta <mathiaslegarreta18@hotmail.com>
2023-10-15 23:16:37 -0300

    arreglo js

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
-let firstCard = Math.floor(Math.random() * (11 - 2 + 1) + 2);
-let secondCard = Math.floor(Math.random() * (11 - 2 + 1) + 2);
-let thirdCard = Math.floor(Math.random() * (11 - 2 + 1) + 2);
-let cards = [firstCard,secondCard]
-let sum = firstCard + secondCard
+
+let cards = [];
+let sum = 0;
 let hasBlackjack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.querySelector("#sum-el");
-let cardsEl = document.querySelector("#cards-el")
+let cardsEl = document.querySelector("#cards-el");
+let playerName = "Mathias";
+let playerChips = 145;
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = playerName + ": $" + playerChips;
+
+function getRandomCard(){
+    let randomNumber = Math.floor(Math.random() * 13) +1;
+
+    if(randomNumber === 1){
+        return 11
+    }else if( randomNumber > 10 ){
+        return 10
+    }else{
+       return randomNumber
+    }    
+}
+
+
 
 function startGame() {
+    let firstCard = getRandomCard() ;
+    let secondCard = getRandomCard() ;
+    cards = [firstCard,secondCard];
+    sum = firstCard + secondCard;
+    isAlive = true;
+
+
     renderGame();
 }
 
 function renderGame() {
     sumEl.textContent = "Resultado: " + sum;
-    cardsEl.textContent = "Cartas: " + cards[0] + " - " + cards[1];
+    cardsEl.textContent = "Cartas: " 
+
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+
     if (sum <= 20) {
         message = "Quieres otra carta?";
     } else if (sum === 21) {
@@ -30,12 +59,17 @@ function renderGame() {
 }
 
 function newCard() {
-    messageEl.textContent = "Entregandote una nueva carta"
+    if (isAlive === true && hasBlackjack === false ){        
+        
+        let thirdCard = getRandomCard();
+        sum += thirdCard;
+        cards.push(thirdCard)
+        console.log(cards);
+        
+        renderGame()
+    }
+    }
 
-    sum += thirdCard;
-
-    renderGame()
 
 
 
-}
